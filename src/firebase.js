aa12475d82601be4543951929857059795db5dbc
angelwebdev <web.angel1993@gmail.com>
2021-01-20 04:30:42 -0800

    matches page

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_hwKHL2hdVOMoxboAiPVp17O_eYxOBWg",
+  authDomain: "the-match-f165b.firebaseapp.com",
+  databaseURL: "https://the-match-f165b-default-rtdb.firebaseio.com",
+  projectId: "the-match-f165b",
+  storageBucket: "the-match-f165b.appspot.com",
+  messagingSenderId: "757681546063",
+  appId: "1:757681546063:web:e97e481509c8226479db3a",
+  measurementId: "G-ZXH1RSMLZ5",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref("matches");
+
+export { firebaseDB, firebaseMatches };
