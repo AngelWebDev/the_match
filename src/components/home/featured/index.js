2e1eb999ac76bbe790d1d28eeb5437a0bf8996ab
angelwebdev <web.angel1993@gmail.com>
2021-01-19 19:31:28 -0800

    complete feature page

--- src/components/home/featured/index.js
+++ src/components/home/featured/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Stripes from "./Stripes";
+import Text from "./Text";
 
 const Featured = () => {
-  return <div className="featured_wrapper">some text</div>;
+  return (
+    <div className="featured_wrapper">
+      <Stripes />
+      <Text />
+    </div>
+  );
 };
 
 export default Featured;
